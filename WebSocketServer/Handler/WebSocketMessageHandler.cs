c6e8fb306746c1b657d11f7dd05e73869e678acc
Yifan Zhuo <im_zyf@outlook.com>
2020-08-12 22:19:34 +0800

    [+]First commit

---
+++ WebSocketServer/Handler/WebSocketMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketServer.SocketsManager;
+
+namespace WebSocketServer.Handler
+{
+    public class WebSocketMessageHandler : SocketHandler
+    {
+        public WebSocketMessageHandler(ConnectionManager connections) : base(connections)
+        {
+        }
+
+        public override Task OnConnected(WebSocket socket)
+        {
+            return base.OnConnected(socket);
+        }
+
+        public override async Task Receive(WebSocket socket, WebSocketReceiveResult result, byte[] buffer)
+        {
+            string socketId = Connections.GetId(socket);
+            await SendMessageExceptId(Encoding.UTF8.GetString(buffer,0,result.Count), socketId);
+        }
+    }
+}
---
+++ WebSocketServer/SocketsManager/SocketsExtention.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace WebSocketServer.SocketsManager
+{
+    public static class SocketsExtention
+    {
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddTransient<ConnectionManager>();
+            foreach (Type type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if (type.GetTypeInfo().BaseType == typeof(SocketHandler))
+                {
+                    services.AddSingleton(type);
+                }
+            }
+
+            return services;
+        }
+
+        public static IApplicationBuilder MapSockets(this IApplicationBuilder app, PathString path,
+            SocketHandler socket)
+        {
+            return app.Map(path, x => x.UseMiddleware<SocketMiddleware>(socket));
+        }
+    }
+}
