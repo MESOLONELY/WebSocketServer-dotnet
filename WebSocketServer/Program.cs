a2ef1b9e8131beba96375e2c3494c1931c6fabde
Yifan Zhuo <im_zyf@outlook.com>
2020-08-30 01:42:57 +0800

    [*] Adapt to https

--- WebSocketServer/Program.cs
+++ WebSocketServer/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebSocketServer
 {
@@ -20,8 +14,26 @@ namespace WebSocketServer
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureKestrel(options => options.ListenAnyIP(5000));
+                    webBuilder.UseStartup<Startup>().UseKestrel(options =>
+                    {
+                        int port = 5443;
+                        if (args.Length > 0)
+                        {
+                            int.TryParse(args[0], out port);
+                        }
+                        if (args.Length > 2)
+                        {
+                            options.ListenAnyIP(port, listenOptions =>
+                            {
+                                listenOptions.UseHttps(args[1], args[2]); // cert.pfx and password
+                            });
+                        }
+                        else
+                        {
+                            options.ListenAnyIP(port);
+                        }
+                    });
+                    // webBuilder.ConfigureKestrel(options => options.ListenAnyIP(5000));
                 });
     }
 }
--- WebSocketServer/Startup.cs
+++ WebSocketServer/Startup.cs
@@ -1,10 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Linq;
-using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -27,6 +21,8 @@ namespace WebSocketServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
+            System.IO.Directory.SetCurrentDirectory(env.ContentRootPath);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
